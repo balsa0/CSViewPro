1bdfa905e97a7ddb019ea58182c933edf0f61164
balsa0 <ktibalazs@gmail.com>
2016-10-23 17:34:51 +0200

    almost finishing file history window

--- src/main/java/com/csviewpro/ui/menu/MainMenuBar.java
+++ src/main/java/com/csviewpro/ui/menu/MainMenuBar.java
@@ -2,10 +2,17 @@ package com.csviewpro.ui.menu;
 
 import com.csviewpro.controller.loader.LoadController;
 import com.csviewpro.controller.util.ImageUtil;
+import javafx.application.Platform;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -20,10 +27,14 @@ public class MainMenuBar extends MenuBar {
 	LoadController loadController;
 
 	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
 	ImageUtil imageUtil;
 
 	private Menu fileMenu = new Menu("Fájl");
 	private MenuItem fileMenu_open = new MenuItem("Megnyitás...");
+	private MenuItem fileMenu_exit = new MenuItem("Kilépés");
 
 	@PostConstruct
 	public void init(){
@@ -38,15 +49,23 @@ public class MainMenuBar extends MenuBar {
 	}
 
 	public void setupFileMenu(){
+		// open menu item
 		fileMenu_open.setGraphic(imageUtil.getResourceIconImage("actions/open_sm.png"));
 		fileMenu_open.setOnAction(event -> {
 			loadController.openFileChooserAction();
 		});
+		fileMenu_open.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN));
 
+		// exit menu item
+		fileMenu_exit.setOnAction(event -> {
+			applicationContext.getBean(Stage.class).close();
+		});
 
 		// populate file menu
 		fileMenu.getItems().addAll(
-				fileMenu_open
+				fileMenu_open,
+				new SeparatorMenuItem(),
+				fileMenu_exit
 		);
 	}
 
--- src/main/java/com/csviewpro/ui/view/FileHistoryView.java
+++ src/main/java/com/csviewpro/ui/view/FileHistoryView.java
@@ -4,8 +4,11 @@ import com.csviewpro.controller.filehandling.FileHistoryController;
 import com.csviewpro.controller.loader.LoadController;
 import com.csviewpro.controller.util.ImageUtil;
 import com.csviewpro.ui.toolbar.FileHistoryToolbar;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.layout.BorderPane;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,12 +44,17 @@ public class FileHistoryView extends BorderPane {
 	private TreeItem<String> favouritesNode;
 	private TreeItem<String> recentsNode;
 
+	// context menus
+	private ContextMenu favouriteContextMenu;
+	private ContextMenu recentContextMenu;
+
 	// internal tree view
 	private TreeView treeView = new TreeView();
 
 	@PostConstruct
 	private void init(){
 		populate();
+		setupContextMenus();
 		updateContent();
 	}
 
@@ -55,6 +63,48 @@ public class FileHistoryView extends BorderPane {
 		setTop(fileHistoryToolbar);
 		// set tree view as central element
 		setCenter(treeView);
+	}
+
+	public void setupContextMenus(){
+		// favourites menu
+		favouriteContextMenu = new ContextMenu();
+		favouriteContextMenu.setConsumeAutoHidingEvents(true);
+
+		MenuItem removeFav = new MenuItem(
+				"Eltávolítás a kedvencekből",
+				imageUtil.getResourceIconImage("actions/cancel_sm.png")
+		);
+		removeFav.setOnAction(event -> {
+			// get selected item
+			String file = (String) (((TreeItem)treeView.getSelectionModel().getSelectedItem()).getValue());
+			// remove from favourites
+
+		});
+
+		// populate favourites context menu
+		favouriteContextMenu.getItems().addAll(
+				removeFav
+		);
+
+		// recents menu
+		recentContextMenu = new ContextMenu();
+		recentContextMenu.setConsumeAutoHidingEvents(true);
+
+		MenuItem addFav = new MenuItem(
+				"Hozzáadás kedvencekhez",
+				imageUtil.getResourceIconImage("actions/star_plus_sm.png")
+		);
+		addFav.setOnAction(event -> {
+			// get selected item
+			String filePath = (String) (((TreeItem)treeView.getSelectionModel().getSelectedItem()).getValue());
+			// add item to favs
+			fileHistoryController.addFavourite(filePath);
+		});
+
+		recentContextMenu.getItems().addAll(
+			addFav
+		);
+
 	}
 
 	public void updateContent(){
@@ -120,11 +170,21 @@ public class FileHistoryView extends BorderPane {
 
 		// set node click actions
 		treeView.setOnMouseClicked(event -> {
+
+			// close context menus
+			favouriteContextMenu.hide();
+			recentContextMenu.hide();
+
 			// only on double click
 			if(event.getClickCount() == 2){
 				// open the selected file
 				openSelectedFile();
 			}
+		});
+
+		// set context menu event
+		treeView.setOnContextMenuRequested(event -> {
+			openContextMenu(event);
 		});
 
 		// add nodes to root node
@@ -150,6 +210,25 @@ public class FileHistoryView extends BorderPane {
 		loadController.openFileAction(
 				new File((String)selected.getValue())
 		);
+	}
+
+	private void openContextMenu(ContextMenuEvent event){
+		// get selected item
+		TreeItem selected = (TreeItem)treeView.getSelectionModel().getSelectedItem();
+
+		// check for special nodes and skip if selected
+		if(selected == rootNode || selected == recentsNode || selected == favouritesNode)
+			return;
+
+		// if selected a recent item
+		if(selected.getParent() == recentsNode){
+			recentContextMenu.show(treeView, event.getScreenX(), event.getScreenY());
+		}
+
+		// if selected a faved item
+		if(selected.getParent() == favouritesNode){
+			favouriteContextMenu.show(treeView, event.getScreenX(), event.getScreenY());
+		}
 	}
 
 }
